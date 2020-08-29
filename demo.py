c342350c70e0e40fcb53e1f8ede9fae56d7c1a8d
Prachi <tholeprachi@gmail.com>
2020-08-29 14:59:55 +0530

    PDR audio reader

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
-print ('hello')
-print('world')
-print('python')
+import pyttsx3\
+import pypdf2
+from tkinter.filedialog import *
+book = askopenfilename(Linux_commnd with example)
+pdfreader = pypdf2.pdfFileReader(book)
+pages = pdfreader.numpages
+for number in range(1,pages):
+page = pdfreader.getPage(number)
+text = page.extractText()
+player = pyttsx3.init()
+player.say(text)
+player.runAndWait()
